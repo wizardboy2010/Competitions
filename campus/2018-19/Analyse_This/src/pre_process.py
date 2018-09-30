42ae0ad09ec0aca652578bd3fd14465fb2d3a248
wizardboy2010 <tsuryateja.iitkgp@gmail.com>
2018-09-30 18:11:09 +0530

    preprocessing

--- campus/2018-19/Analyse_This/src/pre_process.py
+++ campus/2018-19/Analyse_This/src/pre_process.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 from collections import Counter
 
 data = pd.read_csv('campus/2018-19/Analyse_This/data/Training_dataset_Original.csv', low_memory=False)
+
+plt.matshow(data.corr())
+plt.show()
 
 c = Counter(data.mvar1)
