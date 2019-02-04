330459549de476e5fe8669f46db5af21916943cb
wizardboy2010 <tsuryateja.iitkgp@gmail.com>
2019-02-05 00:21:05 +0530

    flipkart competition

--- flipkart/try1.py
+++ flipkart/try1.py
@@ -12,18 +12,15 @@ img = data.read('images/1458171560720DSC_0454.png')
 #print(data.namelist())
 img = cv2.imdecode(np.frombuffer(img, np.uint8), 1)
 
-class getdata(object):
-    
-    def __init__(self, images, labels, one_hot = True):
-        assert images.shape[0] == labels.shape[0]
-        self.num_examples = images.shape[0]
-        assert images.shape[3] == 1
-        self.images = images
-        self.one_hot = one_hot
-        if not one_hot:
-            self.labels = onehot(labels)
+class getdata():
+    def __init__(self, image_names, labels, file_path):
+        assrt len(image_names) == len(labels)
+        self.num_examples = len(image_names)
+        self.names = image_names
+        self.data = {image_names[i]:labels[i] for i in range(len(self.num_examples))}
         self.epoch = 0
         self.startofepoch = 0
+        self.path = file_path
         
     def nextbatch(self, batch_size):
         start = self.startofepoch
@@ -36,10 +33,18 @@ class getdata(object):
             np.random.shuffle(perm)
             
             self.images = self.images[perm]
-            self.labels = self.labels[perm]
             
             start = 0
             end = batch_size
             
         self.startofepoch = end
-        return self.images[start:end], self.labels[start:end]
+        return self.get_img_batch(self.images[start:end]), [self.data[i] for i in self.images[start:end]]
+
+    def get_image(self, data, path):
+    	img = data.read(path)
+    	img = cv2.imdecode(np.frombuffer(img, np.uint8), 1)
+    	return img
+
+    def get_img_batch(self, l):
+    	return [self.get_image(i) for i in l]
+
